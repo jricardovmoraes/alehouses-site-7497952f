bee48ff6b4e6948c3e235997f98d066dc7ab8b64
jricardovmoraes <jricardovmoraes@gmail.com>
2025-11-04 13:44:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/alehouses-site-7497952f/",
   server: {
     host: "::",
     port: 8080,
